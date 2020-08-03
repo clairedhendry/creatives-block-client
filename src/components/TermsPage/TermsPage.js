82338666c23b13b360c383f96b29adb49b2df21e
Claire Hendry <claire.d.hendry@gmail.com>
2020-08-03 11:10:58 -0400

    basic terms of use

--- src/components/TermsPage/TermsPage.js
+++ src/components/TermsPage/TermsPage.js
@@ -8,18 +8,19 @@ export default function Terms() {
         <div className="terms-container">
             <h2>Terms of Use</h2>
             <h3>Registering an Account</h3>
-            <p>Creative's Block requires a user name, password, and email address to sign up.
-                Your information is securely stored and will never be made available to third parties without your consent.</p>
+            <p>Creative's Block requires a user name, password, and email address to sign up.</p>
+            <p>Your information is securely stored and will never be made available to third parties without your consent.</p>
             <h3>Uploading Blocks</h3>
             <p>Creative's Block allows you to upload and share files with the community.
             As a user, you agree to allow Creative's Block to store these files and display them on the website.
             As the creator of your work, you retain all ownership and rights to your content.
-            In using Creative's Block you verify that content uploaded by you is your own and can not be claimed as copyright to someone else.
-                If a user is discovered to be uploading content that they do not own, their account will be terminated.</p>
-            <h4>On Content</h4>
+            In using Creative's Block you verify that content uploaded by you is owned by you.
+            If a user is discovered to be uploading content that they do not own, their account will be terminated.
+            If you suspect a user has uploaded content that is not their own, or done so without permission, please email us at creatives.block@gmail.com</p>
+            <h3>On Content</h3>
             <p>Creative's Block strives to encoruage creativity and a community of healthy feedback.
             Content may be flagged or removed if deemed inappropriate including, but not limited to being offensive,
-                   considered hate-speech, racist, or sexist.</p>
+            considered hate-speech, racist, or sexist.</p>
 
             <h3>Sharing Feedback</h3>
             <p>Feedback is the core of Creative's Block, allowing users to give and receive constructive criticism on projects.
